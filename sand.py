b9e9e2af77625086f7acf2ea38204ea6abc20216
h0axyboi <sabari15krishna@gmail.com>
2020-09-04 23:40:27 +0530

    fine i guess

--- sand.py
+++ sand.py
@@ -1,17 +1,1 @@
-solved:
-rrrrrrrrr ggggggggg ooooooooo bbbbbbbbb yyyyyyyyy wwwwwwwww
-
-tests:
-oyorrwrrr bybgggggg ryroooooo grgbbbbbb yoybygyyy wrwwwwwww
-rrrrrrrrr gggggggwg ooooooooo bbbbbbbbb yyyyyyyyy wgwwwwwww
-wrrrggwwr ggggooogg ooyyybboo bbrrrbbbb ryyyyygry bwwwwwoow
-rrrrrbwrr yyrgooyyg bggwybboo ooggobbgb yywbbwwyy grgwwwoow
-grogorrbr wwobbyyog goryrgrro ywyowrybb bywbbyooy gwbgwwggw
-bwgrrrggr rogbrgywg woyrbbwoo brwyogobb rwoywbogy yygobyyww
-
-#F=list(input().upper())
-#R=list(input().upper())
-#B=list(input().upper())
-#L=list(input().upper())
-#U=list(input().upper())
-#D=list(input().upper())
+print(list(range(6,5)))
